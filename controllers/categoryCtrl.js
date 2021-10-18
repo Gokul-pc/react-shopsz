33380e6d038feaba44e985cab9cf2fb718e0a4c3
Gokul-pc <gokulpc855@gmail.com>
2021-10-18 14:54:07 +0530

    delete,update,create added to category

--- controllers/categoryCtrl.js
+++ controllers/categoryCtrl.js
@@ -20,7 +20,24 @@ const categoryCtrl = {
             if (category) return res.status(400).json({ msg: " this category already exist" })
             const newCategory = new Category({ name })
             await newCategory.save()
-            res.json({msg: "created a category"})
+            res.json({ msg: "created a category" })
+        } catch (err) {
+            return res.status(500).json({ msg: err.message })
+        }
+    },
+    deleteCategory: async (req, res) => {
+        try {
+            await Category.findByIdAndDelete(req.params.id)
+            res.json({ msg: "Deleted a category" })
+        } catch (err) {
+            return res.status(500).json({ msg: err.message })
+        }
+    },
+    updateCategory: async (req, res) => {
+        try {
+            const {name}=req.body
+            await Category.findOneAndUpdate({_id: req.params.id}, {name})
+            res.json({ msg: "Updated a category" })
         } catch (err) {
             return res.status(500).json({ msg: err.message })
         }
