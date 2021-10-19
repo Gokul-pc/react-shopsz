a5dc8aa2d8629b7d297aa02986be6f2c4b0fa88b
Gokul-pc <gokulpc855@gmail.com>
2021-10-19 13:12:36 +0530

    image uploading to cloudinary added

--- routes/upload.js
+++ routes/upload.js
@@ -15,9 +15,21 @@ router.post('/upload', (req, res) => {
     try {
         console.log(req.files);
         if (!req.files || Object.keys(req.files).length === 0)
-            return res.status(400).send('No files were uploded.')
-        const file=req.files.file
-        res.json('test upload')
+            return res.status(400).json({ msg: 'No files were uploded.' })
+
+        const file = req.files.file
+
+        if (file.size > 1024 * 1024)
+            return res.status(400).json({ msg: "file size is too large" })
+
+        if (file.mimetype !== 'image/jpeg' && file.mimetype !== 'image/png')
+            return res.status(400).json({ msg: "file format is incorrect." })
+
+        cloudinary.v2.uploader.upload(file.tempFilePath, { folder: 'test' }, async (err, result) => {
+            if (err) throw err
+            res.json({ result })
+        })
+        
     } catch (err) {
         res.status(500).json({ msg: err.message })
     }
--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.use(expres.json())
 app.use(cookieParser())
 app.use(cors())
 app.use(fileUpload({
+   useTempFiles:true,
    
 }))
 
