68d3ca971309861f3375e739480f11250c5072db
Gokul-pc <gokulpc855@gmail.com>
2021-10-20 16:36:01 +0530

    filtering and sorting added

--- controllers/productCtrl.js
+++ controllers/productCtrl.js
@@ -1,9 +1,35 @@
 const Products = require('../models/productModel')
 
+
+class APIfeature {
+    constructor(query, queryString) {
+        this.query = query
+        this.queryString = queryString
+    }
+    filtering() {
+        const queryObj = { ...this.queryString } //queryString = req.body
+        console.log({ before: queryObj }); //before delete page
+
+        const excludeFields = ['page', 'sort', 'limit']
+        excludeFields.forEach(el => delete (queryObj[el]))
+        console.log({ after: queryObj }); //after delete page
+
+        let queryStr = JSON.stringify(queryObj)
+        queryStr = queryStr.replace(/\b(gte|gt|lt|lte|regex)\b/g, match =>`$${match}`)
+        console.log({ queryObj, queryStr });
+        return this
+    }
+
+    sorting() { }
+
+    paginating() { }
+}
+
 const productCtrl = {
     getProducts: async (req, res) => {
         try {
-            const products = await Products.find()
+            const features = new APIfeature(Products.find(), req.query).filtering()
+            const products = await features.query
             res.json(products)
         } catch (err) {
             return res.status(500).json({ msg: err.message })
@@ -36,11 +62,11 @@ const productCtrl = {
     updateProducts: async (req, res) => {
         try {
             const { title, price, description, content, images, category } = req.body
-            if(!images) return res.status(400).json({ msg: "no image uploaded" })
-            await Products.findOneAndUpdate({_id: req.params.id},{
-                title:title.toLowerCase(),price,description,content,images,category
+            if (!images) return res.status(400).json({ msg: "no image uploaded" })
+            await Products.findOneAndUpdate({ _id: req.params.id }, {
+                title: title.toLowerCase(), price, description, content, images, category
             })
-            res.json({msg:"Updated a product"})
+            res.json({ msg: "Updated a product" })
         } catch (err) {
             return res.status(500).json({ msg: err.message })
         }
